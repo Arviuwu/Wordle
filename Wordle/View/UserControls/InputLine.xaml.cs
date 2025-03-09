0554f494c81edcb47b2ee025a348a3aab5ad92c8
Arviuwu <arviu23@gmail.com>
2025-03-09 20:21:29 +0100

    Add project files.

---
+++ Wordle/View/UserControls/InputLine.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Wordle.View.UserControls
+{
+    /// <summary>
+    /// Interaction logic for InputLine.xaml
+    /// </summary>
+    public partial class InputLine : UserControl
+    {
+        public InputLine()
+        {
+            InitializeComponent();
+        }
+    }
+}
