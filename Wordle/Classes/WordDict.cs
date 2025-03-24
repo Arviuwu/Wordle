7df22d1d82e3b2ca6b5b423571c583ae37586519
Arviu <arviu23@gmail.com>
2025-03-24 13:32:59 +0100

    a

--- Wordle/Classes/WordDict.cs
+++ Wordle/Classes/WordDict.cs
@@ -27,6 +27,7 @@ namespace Wordle.Classes
         static public string PathAllWords = Path.Combine(AppDomain.CurrentDomain.BaseDirectory,"..","..","..", "Files", "five_letter_words.txt");
         static public string PathGuessWords = Path.Combine(AppDomain.CurrentDomain.BaseDirectory,"..","..","..", "Files", "guess_words.txt");
         static public Action? OnLineCompleted;
+        static public Action? GameWon;
         static public string? currentGuess;
         static public List<string> guessableWords = new List<string>();
         static public string? currentWord = "0";
